11a8429f61a2e127fee8f9ebfebc0e2558c462ba
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2022-12-03 14:44:39 +0300

    Refact day 02

--- src/zelark/aoc_2022/day_02.clj
+++ src/zelark/aoc_2022/day_02.clj
@@ -9,28 +9,27 @@
 
 (defn parse-input [input]
   (->> (str/split-lines input)
-       (map #(str/split % #" "))))
+       (map (fn [[a _ b]] [a b]))))
 
 (def letter->shape
-  {"A" :rock "B" :paper "C" :scissors
-   "X" :rock "Y" :paper "Z" :scissors})
+  {\A :rock \B :paper \C :scissors
+   \X :rock \Y :paper \Z :scissors})
 
 (defn score [state shape]
   (+ (get {:rock 1 :paper 2 :scissors 3} shape)
      (get {:loss 0 :draw 3 :win 6} state)))
 
+(defn state [[a b :as choice]]
+  (cond
+    (= a b)                       :draw
+    (= choice [:scissors :rock])  :win
+    (= choice [:rock :paper])     :win
+    (= choice [:paper :scissors]) :win
+    :else                         :loss))
+
 ;; part 1
-(defn score-p1 [choice]
-  (case choice
-    [:paper    :rock]     (score :loss :rock)
-    [:scissors :paper]    (score :loss :paper)
-    [:rock     :scissors] (score :loss :scissors)
-    [:rock     :rock]     (score :draw :rock)
-    [:paper    :paper]    (score :draw :paper)
-    [:scissors :scissors] (score :draw :scissors)
-    [:scissors :rock]     (score :win :rock)
-    [:rock     :paper]    (score :win :paper)
-    [:paper    :scissors] (score :win :scissors)))
+(defn score-p1 [[_ b :as choice]]
+  (score (state choice) b))
 
 (->> (parse-input input)
      (map #(map letter->shape %))
@@ -40,15 +39,15 @@
 (defn score-p2 [[a b]]
   (let [shape (letter->shape a)]
     (case b
-      "X" (case shape
-            :rock     (score :loss :scissors)
-            :paper    (score :loss :rock)
-            :scissors (score :loss :paper))
-      "Y" (score :draw shape)
-      "Z" (case shape
-            :rock     (score :win :paper)
-            :paper    (score :win :scissors)
-            :scissors (score :win :rock)))))
+      \X (case shape
+           :rock     (score :loss :scissors)
+           :paper    (score :loss :rock)
+           :scissors (score :loss :paper))
+      \Y (score :draw shape)
+      \Z (case shape
+           :rock     (score :win :paper)
+           :paper    (score :win :scissors)
+           :scissors (score :win :rock)))))
 
 (->> (parse-input input)
      (transduce (map score-p2) +)) ; 14979

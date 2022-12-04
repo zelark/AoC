e68537e9a81c95f014f62b35911e8008e40a76c5
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2022-12-04 11:58:27 +0300

    Change get-input so it loads input if it's not found

--- src/zelark/aoc/core.clj
+++ src/zelark/aoc/core.clj
@@ -1,12 +1,15 @@
 (ns zelark.aoc.core
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.java.shell :as shell]))
 
 (defn get-input [year day]
-  (-> (format "%d/input_%02d.txt" year day)
-      (io/resource)
-      (slurp)
-      (str/trim-newline)))
+  (let [path-to-file (format "%d/input_%02d.txt" year day)
+        load-input #(-> % io/resource slurp str/trim-newline)]
+    (if (.exists (io/file path-to-file))
+      (load-input path-to-file)
+      (do (shell/sh "./bin/fetch-input" (str year) (str day))
+          (load-input path-to-file)))))
 
 ;; Parsing
 (defn parse-longs [s]

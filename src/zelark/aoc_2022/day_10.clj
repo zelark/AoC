c8668d3cdcf2ce684e140df9c366ab8770576d27
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2022-12-10 11:25:15 +0300

    Refact Day 10: Cathode-Ray Tube

--- src/zelark/aoc_2022/day_10.clj
+++ src/zelark/aoc_2022/day_10.clj
@@ -8,30 +8,27 @@
 (def input (aoc/get-input 2022 10))
 
 (defn run [code f]
-  (loop [state {:x 1, :mem []}, ip 0, cycle 0]
-    (let [[op x] (nth code ip [:hlt])]
-      (case op
-        :hlt (:mem state)
-        :noop (let [cycle (inc cycle)
-                    state (f state cycle)]
-                (recur state (inc ip) cycle))
-        :addx (let [cycle (inc cycle)
-                    state (f state cycle)
-                    cycle (inc cycle)
-                    state (f state cycle)]
-               (recur (update state :x + x) (inc ip) cycle))))))
+  (->> code
+       (reduce (fn [state [op x]]
+                 (case op
+                   :noop (f (update state :cycle inc))
+                   :addx (let [state (f (update state :cycle inc))
+                               state (f (update state :cycle inc))]
+                           (update state :x + x))))
+               {:cycle 0, :x 1, :mem []})
+       :mem))
 
 ;; part 1
-(defn signal-strength [state cycle]
+(defn signal-strength [{:keys [x cycle] :as state}]
   (cond-> state
     (contains? #{20 60 100 140 180 220} cycle)
-    (update :mem conj (* cycle (:x state)))))
+    (update :mem conj (* cycle x))))
 
 (->> (run (aoc/parse-asm-code input) signal-strength)
      (reduce +)) ; 11720
 
 ;; part 2
-(defn draw-pixel [{:keys [x] :as state} cycle]
+(defn draw-pixel [{:keys [x cycle] :as state}]
   (let [pos (mod (dec cycle) 40)]
     (if (some #{pos} [(dec x) x (inc x)])
       (update state :mem conj \#)
@@ -40,10 +37,3 @@
 (->> (run (aoc/parse-asm-code input) draw-pixel)
      (partition 40)
      (map #(str/join %))) ; ERCREPCJ
-
-;; ("####.###...##..###..####.###...##....##."
-;;  "#....#..#.#..#.#..#.#....#..#.#..#....#."
-;;  "###..#..#.#....#..#.###..#..#.#.......#."
-;;  "#....###..#....###..#....###..#.......#."
-;;  "#....#.#..#..#.#.#..#....#....#..#.#..#."
-;;  "####.#..#..##..#..#.####.#.....##...##..")

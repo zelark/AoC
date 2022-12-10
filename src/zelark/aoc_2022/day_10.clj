fb9c3abd636d43103bcb193ba3561114661773e0
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2022-12-10 16:38:08 +0300

    Refact Day 10: Cathode-Ray Tube
    Inspired by @tschady's solution.

--- src/zelark/aoc_2022/day_10.clj
+++ src/zelark/aoc_2022/day_10.clj
@@ -7,33 +7,24 @@
 
 (def input (aoc/get-input 2022 10))
 
-(defn run [code f]
-  (->> code
-       (reduce (fn [state [op x]]
-                 (case op
-                   :noop (f (update state :cycle inc))
-                   :addx (let [state (f (update state :cycle inc))
-                               state (f (update state :cycle inc))]
-                           (update state :x + x))))
-               {:cycle 0, :x 1, :mem []})
-       :mem))
+(defn parse-input [input]
+  (->> (str/replace input #"noop|addx" "0")
+       (aoc/parse-longs)
+       (reductions + 1))) ; add 1 as init state of register x.
 
 ;; part 1
-(defn signal-strength [{:keys [x cycle] :as state}]
-  (cond-> state
-    (contains? #{20 60 100 140 180 220} cycle)
-    (update :mem conj (* cycle x))))
-
-(->> (run (aoc/parse-asm-code input) signal-strength)
-     (reduce +)) ; 11720
+(let [xs (vec (cons 0 (parse-input input)))] ; add zero to align with cycles
+  (->> (range 20 260 40)
+       (map #(* % (xs %)))
+       (reduce +))) ; 11720
 
 ;; part 2
-(defn draw-pixel [{:keys [x cycle] :as state}]
-  (let [pos (mod (dec cycle) 40)]
-    (if (some #{pos} [(dec x) x (inc x)])
-      (update state :mem conj \#)
-      (update state :mem conj \.))))
+(defn ->px [cycle pos]
+  (let [delta    (- (mod cycle 40) pos)
+        visible? (<= -1 delta 1)]
+    (if visible? \# \.)))
 
-(->> (run (aoc/parse-asm-code input) draw-pixel)
+(->> (parse-input input)
+     (map-indexed ->px)
      (partition 40)
      (map #(str/join %))) ; ERCREPCJ

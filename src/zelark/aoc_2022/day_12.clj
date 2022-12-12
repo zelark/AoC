d305136cddb76e9bd3ac5ec18d15eaa31f633956
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2022-12-12 11:24:59 +0300

    Add Day 12: Hill Climbing Algorithm

---
+++ src/zelark/aoc_2022/day_12.clj
@@ -0,0 +1,41 @@
+(ns zelark.aoc-2022.day-12
+  (:require [zelark.aoc.core :as aoc]
+            [clojure.string :as str]))
+
+;; --- Day 12: Hill Climbing Algorithm ---
+;; https://adventofcode.com/2022/day/12
+
+(def input (aoc/get-input 2022 12))
+
+(defn parse-input [input]
+  (->> (str/split-lines input)
+       (mapcat (fn [y line] (map-indexed (fn [x ch] [[x y] ch]) line)) (range))
+       (reduce (fn [m [loc h]]
+                 (case h
+                   \S (assoc m loc \a, :start loc)
+                   \E (assoc m loc \z, :end loc)
+                   (assoc m loc h)))
+               {})))
+
+(defn can-move? [p1 p2]
+  (<= (- (int p2) (int p1)) 1))
+
+(defn moves [heightmap [x y :as src]]
+  (for [dst [[x (dec y)] [(inc x) y] [x (inc y)] [(dec x) y]]
+        :when (heightmap dst)
+        :when (can-move? (heightmap src) (heightmap dst))]
+    dst))
+
+(def heightmap (parse-input input))
+
+;; part 1
+(count (aoc/bfs (partial moves heightmap)
+                (:start heightmap)
+                (:end heightmap))) ; 528
+
+;; part 2
+(->> (for [start (reduce-kv (fn [acc loc h] (if (= h \a) (conj acc loc) acc)) [] heightmap)
+           :let  [path (aoc/bfs (partial moves heightmap) start (:end heightmap))]
+           :when (some? path)] ; Only walkable paths.
+       (count path))
+     (apply min))  ; 522

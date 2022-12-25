e7720414ed8e47e7cd51b8065ea3bf79f7ffe874
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2022-12-25 12:26:53 +0300

    Add Day 25: Full of Hot Air

---
+++ src/zelark/aoc_2022/day_25.clj
@@ -0,0 +1,34 @@
+(ns zelark.aoc-2022.day-25
+  (:require [zelark.aoc.core :as aoc]
+            [clojure.string :as str]))
+
+;; --- Day 25: Full of Hot Air ---
+;; https://adventofcode.com/2022/day/25
+
+(def input (aoc/get-input 2022 25))
+
+(defn parse [input]
+  (str/split-lines input))
+
+(defn snafu->dec [snafu]
+  (->> (map {\2 2 \1 1 \0 0 \- -1 \= -2} snafu)
+       (reduce (fn [n d] (+ (* n 5) d)) 0)))
+
+(defn dec->snafu [decimal]
+  (loop [n   decimal
+         ret ()]
+    (if (zero? n)
+      (str/join ret)
+      (let [[n r] ((juxt quot rem) n 5)]
+        (recur (+ n ({3 1 4 1} r 0))
+               (cons ({3 \= 4 \-} r r) ret))))))
+
+;; part 1
+(->> (parse input)
+     (map snafu->dec)
+     (aoc/sum)
+     (dec->snafu)) ; => 2=-0=01----22-0-1-10
+
+;; part 2
+;; With all 50 stars we can start the blender, and make a smoothie
+;; to the reindeer.

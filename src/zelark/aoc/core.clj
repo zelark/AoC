226b7594bde4a3768a536ca9476f072f676c05c8
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2023-12-09 08:59:31 +0300

    Add take-until to core

--- src/zelark/aoc/core.clj
+++ src/zelark/aoc/core.clj
@@ -23,6 +23,17 @@
   [f coll]
   (persistent! (reduce #(assoc! %1 (f %2) %2) (transient {}) coll)))
 
+(defn take-until
+  "Returns a lazy sequence of successive items from coll until
+   (pred item) returns true, including that item. pred must be
+   free of side-effects."
+  [pred coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (if (pred (first s))
+       (cons (first s) nil)
+       (cons (first s) (take-until pred (rest s)))))))
+
 ;; Parsing
 (defn parse-longs [s]
   (->> (re-seq #"-?\d+" s)

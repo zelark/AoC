4f1c3b3f254a568d441ff633def3ae55dbed0377
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2023-12-09 21:15:37 +0300

    [2023 Day 2] Polish parsing and part 2

--- src/zelark/aoc_2023/day_02.clj
+++ src/zelark/aoc_2023/day_02.clj
@@ -7,44 +7,40 @@
 
 (def input (aoc/get-input 2023 02))
 
+(defn parse-subsets [subsets]
+  (->> (str/split subsets #";")
+       (map #(re-seq #"(\d+)\s(red|green|blue)" %))
+       (mapv (fn [subset]
+               (reduce (fn [m [_ n color]]
+                         (assoc m (keyword color) (parse-long n)))
+                       {}
+                       subset)))))
+
 (defn parse-line [line]
-  (let [[game line] (str/split line #":")
-        sets        (str/split line #";")]
-    {:game (parse-long (re-find #"\d+" game))
-     :sets
-     (->> (map #(re-seq #"(\d+)\s([a-z]+)" %) sets)
-          (mapv (fn [set]
-                  (reduce (fn [m [_ n color]]
-                            (assoc m (keyword color) (parse-long n)))
-                          {}
-                          set))))}))
+  (let [[_ game subsets] (re-find #"Game (\d+): (.+)" line)]
+    {:game    (parse-long game)
+     :subsets (parse-subsets subsets)}))
 
 (defn parse-input [input]
   (->> (str/split-lines input)
        (map parse-line)))
 
-;; part 1
 (defn possible?
-  "only 12 red cubes, 13 green cubes, and 14 blue cubes"
+  "Only 12 red cubes, 13 green cubes, and 14 blue cubes."
   [{:keys [red green blue]
     :or {red 0 green 0 blue 0}}]
   (and (<= red   12)
        (<= green 13)
        (<= blue  14)))
 
+;; part 1
 (->> (parse-input input)
-     (filter (fn [{:keys [sets]}] (every? possible? sets)))
+     (filter (fn [{:keys [subsets]}] (every? possible? subsets)))
      (reduce (fn [acc {:keys [game]}] (+ acc game)) 0)) ; 2076
 
 ;; part 2
 (->> (parse-input input)
-     (map (fn [{:keys [sets]}]
-            (reduce (fn [[r g b] {:keys [red green blue]
-                                  :or {red 0 green 0 blue 0}}]
-                      [(max r red)
-                       (max g green)
-                       (max b blue)])
-                    [0 0 0]
-                    sets)))
-     (reduce (fn [acc [r g b]]
-               (+ acc (* r g b))) 0)) ; 70950
+     (map (fn [{:keys [subsets]}]
+            (reduce #(merge-with max %1 %2) subsets)))
+     (reduce (fn [acc {:keys [red green blue]}]
+               (+ acc (* red green blue))) 0)) ; 70950

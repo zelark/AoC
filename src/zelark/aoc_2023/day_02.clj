1db4005359df36fa67e57c797e6e298c57d66f89
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2023-12-10 00:29:26 +0300

    [2023 Day 2] Refact it with parse-clines

--- src/zelark/aoc_2023/day_02.clj
+++ src/zelark/aoc_2023/day_02.clj
@@ -1,46 +1,43 @@
 (ns zelark.aoc-2023.day-02
-  (:require [zelark.aoc.core :as aoc]
-            [clojure.string :as str]))
+  (:require [zelark.aoc.core :as aoc]))
 
 ;; --- Day 2: Cube Conundrum ---
 ;; https://adventofcode.com/2023/day/2
 
 (def input (aoc/get-input 2023 02))
 
-(defn parse-subsets [subsets]
-  (->> (str/split subsets #";")
-       (map #(re-seq #"(\d+)\s(red|green|blue)" %))
-       (mapv (fn [subset]
-               (reduce (fn [m [_ n color]]
-                         (assoc m (keyword color) (parse-long n)))
-                       {}
-                       subset)))))
+(defn parse-cubes [line]
+  (->> (re-seq #"(\d+) (red|green|blue)" line)
+       (reduce (fn [m [_ n color]]
+                 (assoc m (keyword color) (parse-long n)))
+               {})))
 
-(defn parse-line [line]
-  (let [[_ game subsets] (re-find #"Game (\d+): (.+)" line)]
-    {:game    (parse-long game)
-     :subsets (parse-subsets subsets)}))
-
-(defn parse-input [input]
-  (->> (str/split-lines input)
-       (map parse-line)))
+(defn prepare [games]
+  (reduce-kv (fn [m id cubes]
+               (assoc m id (reduce #(merge-with max %1 %2) cubes)))
+             {}
+             games))
 
 (defn possible?
   "Only 12 red cubes, 13 green cubes, and 14 blue cubes."
-  [{:keys [red green blue]
-    :or {red 0 green 0 blue 0}}]
+  [{:keys [red green blue] :or {red 0 green 0 blue 0}}]
   (and (<= red   12)
        (<= green 13)
        (<= blue  14)))
 
+(defn solve [f input]
+  (->> (aoc/parse-clines input #";" parse-cubes)
+       (prepare)
+       (reduce-kv f 0)))
+
 ;; part 1
-(->> (parse-input input)
-     (filter (fn [{:keys [subsets]}] (every? possible? subsets)))
-     (reduce (fn [acc {:keys [game]}] (+ acc game)) 0)) ; 2076
+(defn part1 [acc id cubes]
+  (if (possible? cubes) (+ acc id) acc))
+
+(solve part1 input) ; 2076
 
 ;; part 2
-(->> (parse-input input)
-     (map (fn [{:keys [subsets]}]
-            (reduce #(merge-with max %1 %2) subsets)))
-     (reduce (fn [acc {:keys [red green blue]}]
-               (+ acc (* red green blue))) 0)) ; 70950
+(defn part2 [acc _ cubes]
+  (+ acc (apply * (vals cubes))))
+
+(solve part2 input) ; 70950

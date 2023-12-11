9a8ad1a5fa30a60b0cb6770293458683a7364293
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2023-12-11 11:00:20 +0300

    [2023 Day 11] Refact solution

--- src/zelark/aoc_2023/day_11.clj
+++ src/zelark/aoc_2023/day_11.clj
@@ -15,10 +15,7 @@
      :cols (keep-indexed (fn [c l] (when (every? #(= % \.) l) c)) (aoc/transpose lines))}))
 
 (defn in-range? [a b c]
-  (cond
-    (== a b) false
-    (< a b)  (< a c b)
-    (< b a)  (< b c a)))
+  (< (min a b) c (max a b)))
 
 (defn solve [input t]
   (let [t (dec t)
@@ -26,11 +23,10 @@
         {empty-rows :rows empty-cols :cols} (empty-space input)]
     (->> (comb/combinations galaxies 2)
          (map (fn [[[x1 y1] [x2 y2]]]
-                (let [xx (* (count (filter #(in-range? x1 x2 %) empty-cols)) t)
-                      yy (* (count (filter #(in-range? y1 y2 %) empty-rows)) t)
-                      [x1 x2] (if (< x1 x2) [x1 (+ x2 xx)] [(+ x1 xx) x2])
-                      [y1 y2] (if (< y1 y2) [y1 (+ y2 yy)] [(+ y1 yy) y2])]
-                  (aoc/manhattan-distance [x1 y1] [x2 y2]))))
+                (let [xx (count (filter #(in-range? x1 x2 %) empty-cols))
+                      yy (count (filter #(in-range? y1 y2 %) empty-rows))]
+                  (+ (aoc/manhattan-distance [x1 y1] [x2 y2])
+                     (* (+ xx yy) t)))))
          (aoc/sum))))
 
 ;; part 1

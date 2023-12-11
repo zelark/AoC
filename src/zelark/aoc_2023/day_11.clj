24af3a980eee121f8ebc5e42e07b78f910f3beaf
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2023-12-11 13:42:28 +0300

    [2023 Day 11] Simplify it

--- src/zelark/aoc_2023/day_11.clj
+++ src/zelark/aoc_2023/day_11.clj
@@ -1,7 +1,6 @@
 (ns zelark.aoc-2023.day-11
   (:require [zelark.aoc.core :as aoc]
             [zelark.aoc.grid-2d :as g2]
-            [clojure.string :as str]
             [clojure.math.combinatorics :as comb]))
 
 ;; --- Day 11: Cosmic Expansion ---
@@ -9,22 +8,19 @@
 
 (def input (aoc/get-input 2023 11))
 
-(defn empty-space [input]
-  (let [lines (str/split-lines input)]
-    {:rows (keep-indexed (fn [r l] (when (re-find #"^\.+$" l) r)) lines)
-     :cols (keep-indexed (fn [c l] (when (every? #(= % \.) l) c)) (aoc/transpose lines))}))
-
-(defn in-range? [a b c]
-  (< (min a b) c (max a b)))
+(defn spaces [xs x1 x2]
+  (->> (range (min x1 x2) (max x1 x2))
+       (reduce (fn [n x] (if (xs x) n (inc n))) 0)))
 
 (defn solve [input t]
   (let [t (dec t)
         galaxies (keys (g2/parse input #(= % \#)))
-        {empty-rows :rows empty-cols :cols} (empty-space input)]
+        xs (set (map first galaxies))
+        ys (set (map second galaxies))]
     (->> (comb/combinations galaxies 2)
          (map (fn [[[x1 y1] [x2 y2]]]
-                (let [xx (count (filter #(in-range? x1 x2 %) empty-cols))
-                      yy (count (filter #(in-range? y1 y2 %) empty-rows))]
+                (let [xx (spaces xs x1 x2)
+                      yy (spaces ys y1 y2)]
                   (+ (aoc/manhattan-distance [x1 y1] [x2 y2])
                      (* (+ xx yy) t)))))
          (aoc/sum))))

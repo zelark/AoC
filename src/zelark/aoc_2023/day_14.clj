992446f3c1737d56e762f8d8d70337a5dd8f334e
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2023-12-31 01:56:26 +0300

    [2023 Day 14] Add solution

--- src/zelark/aoc_2023/day_14.clj
+++ src/zelark/aoc_2023/day_14.clj
@@ -11,49 +11,50 @@
 (defn parse [input]
   (str/split-lines input))
 
-(defn tilt-row [row]
-  (loop [x   (first row)
-         row (rest row)
-         res []
-         space []]
-    (if-not x
-      (str/join (into res space))
-      (case x
-        \. (recur (first row) (rest row) res (conj space x))
-        \O (recur (first row) (rest row) (conj res x) space)
-        \# (recur (first row) (rest row) (conj (into res space) x) [])))))
+(defn tilt-part [^String part dir]
+  (if (str/blank? part)
+    part
+    (let [rocks (.repeat "O" (aoc/cnt part \O))
+          space (.repeat "." (aoc/cnt part \.))]
+      (case dir
+        :left  (str rocks space)
+        :right (str space rocks)))))
 
-(defn tilt [panel]
-  (mapv tilt-row panel))
+(defn tilt-row [^String row dir]
+  (->> (str/split row #"#" -1)
+       (map #(tilt-part % dir))
+       (str/join "#")))
 
-(defn calc-load [panel]
-  (let [len (count panel)]
-    (->> (map-indexed (fn [i row]
-                        (reduce #(if (= %2 \O) (+ %1 (- len i)) %1) 0 row))
-                      panel)
-         (aoc/sum))))
+(defn tilt [platform dir]
+  (mapv #(tilt-row % dir) platform))
 
-;; part 1
+(defn calc-load [platform]
+  (->> (map #(* (aoc/cnt %1 \O) %2) platform (range (count platform) 0 -1))
+       (aoc/sum)))
+
+;; part 1 (3.449105 msecs)
 (let [panel (parse input)]
-  (->> (aoc/transpose panel)
-       (tilt)
-       (aoc/transpose)
-       (calc-load))) ; 105623
+  (-> panel aoc/rotate-ccw (tilt :left) aoc/rotate-cw calc-load)) ; 105623
 
-;; part 2
-(defn spin-cycle [tile]
-  (-> tile tilt aoc/rotate tilt aoc/rotate tilt aoc/rotate tilt aoc/rotate))
+;; part 2 (730.663208 msecs)
+(defn spin-cycle [platform]
+  (-> platform
+      aoc/rotate-ccw (tilt :left)    ; north
+      aoc/rotate-ccw (tilt :right)   ; west
+      aoc/rotate-ccw (tilt :left)    ; south
+      aoc/rotate-ccw (tilt :right))) ; east
 
-(let [panel (parse input)
-      anti-rotate (comp aoc/rotate aoc/rotate aoc/rotate)
-      [i j seen] (->> (anti-rotate panel)
-                      (iterate spin-cycle)
-                      (map-indexed vector)
-                      (reduce (fn [seen [idx p]] (if (seen p) (reduced [(seen p) idx seen]) (assoc seen p idx))) {}))
-      n (mod (- 1000000000 (dec i))
-             (- j i))]
-  (-> ((set/map-invert seen) (+ (dec i) n))
-      (rotate)
-      (calc-load))) ; 98029
+(defn final-state [start end state->cycle-id]
+  (let [left (mod (- 1000000000 end) (- end start))
+        id (+ start left)]
+    ((set/map-invert state->cycle-id) id)))
 
-;; "Elapsed time: 3754.234856 msecs"
+(let [panel (parse input)]
+  (->> (iterate spin-cycle panel)
+       (map-indexed vector)
+       (reduce (fn [seen [cycle-id state]]
+                 (if-let [start (seen state)]
+                   (reduced (final-state start cycle-id seen))
+                   (assoc seen state cycle-id)))
+               {})
+       (calc-load))) ; 98029

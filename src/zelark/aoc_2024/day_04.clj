d56169c23180927216dfd853dc201ca8cc81203f
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2024-12-04 09:08:32 +0300

    [2024] Add day 04

---
+++ src/zelark/aoc_2024/day_04.clj
@@ -0,0 +1,56 @@
+(ns zelark.aoc-2024.day-04
+  (:require [zelark.aoc.core :as aoc]
+            [zelark.aoc.grid-2d :as g2]))
+
+;; --- Day 4: Ceres Search ---
+;; https://adventofcode.com/2024/day/4
+
+(def input (aoc/get-input 2024 4))
+
+(defn parse-input [input]
+  (g2/parse input))
+
+(defn xmas-locs [[x y]]
+  (for [dx [-1 0 1]
+        dy [-1 0 1]
+        :when (not= 0 dx dy)]
+    [[(+ x dx) (+ y dy)]
+     [(+ x dx dx) (+ y dy dy)]
+     [(+ x dx dx dx) (+ y dy dy dy)]]))
+
+(defn letters [grid locs]
+  (map grid locs))
+
+;; part 1 (79.665425 msecs)
+(let [grid (parse-input input)]
+  (reduce-kv (fn [acc loc ch]
+               (if (= ch \X)
+                 (-> (map #(letters grid %) (xmas-locs loc))
+                     (aoc/cnt [\M \A \S])
+                     (+ acc))
+                 acc))
+             0
+             grid)) ; 2427
+
+;; part 2 (91.289061 msecs)
+(defn mas-locs [[x y]]
+  (for [dx [-1 1]
+        dy [-1 1]
+        :when (not= 0 dx dy)]
+    [[(+ x dx) (+ y dy)]
+     [(+ x dx dx) (+ y dy dy)]]))
+
+(let [grid (parse-input input)]
+  (-> (reduce-kv (fn [acc loc ch]
+                   (if (= ch \M)
+                     (->> (mas-locs loc)
+                          (map #(-> [(first %) (letters grid %)]))
+                          (filter #(= (second %) [\A \S]))
+                          (map first)
+                          (into acc))
+                     acc))
+                 []
+                 grid)
+      (frequencies)
+      (vals)
+      (aoc/cnt 2))) ; 1900

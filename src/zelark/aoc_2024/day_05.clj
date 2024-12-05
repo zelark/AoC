7dda2b178b0800b823657f754918070a1dcc34e4
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2024-12-05 11:17:53 +0300

    [2024] Polish Day 05

--- src/zelark/aoc_2024/day_05.clj
+++ src/zelark/aoc_2024/day_05.clj
@@ -10,9 +10,7 @@
 (defn parse-rules [rules]
   (->> (str/split-lines rules)
        (mapv aoc/parse-longs)
-       (reduce (fn [m [a b]]
-                 (update m a (fnil conj #{}) b))
-               {})))
+       (reduce (fn [m [a b]] (update m a (fnil conj #{}) b)) {})))
 
 (defn parse-input [input]
   (let [[rules updates] (aoc/split-on-blankline input)]
@@ -21,25 +19,20 @@
                    (mapv aoc/parse-longs))}))
 
 ;; part 1 (6.065864 msecs)
-(defn correct-order? [rules [current & left-pages]]
-  (cond
-    (empty? left-pages) true
-
-    (some #((get rules % #{}) current) left-pages)
-    false
-
-    :else (recur rules left-pages)))
-
-(defn sum-middle-page-numbers [updates]
-  (let [middle #(quot (count %) 2)]
-    (aoc/sum #(nth % (middle %)) updates)))
+(defn correct-order? [rules pages]
+  (boolean (reduce (fn [prev current]
+                     (if (some prev (rules current))
+                       (reduced false)
+                       (conj prev current)))
+                   #{}
+                   pages)))
 
 (let [{:keys [rules updates]} (parse-input input)]
   (->> (filter #(correct-order? rules %) updates)
-       (sum-middle-page-numbers))) ; 6949
+       (aoc/sum aoc/middle))) ; 6949
 
 ;; part 2 (6.637427 msecs)
-(defn cmp [rules]
+(defn page-cmp [rules]
   (fn [a b]
     (cond
       ((rules a #{}) b) -1
@@ -47,10 +40,10 @@
       :else              0)))
 
 (defn fix-order [rules pages]
-  (sort (cmp rules) pages))
+  (sort (page-cmp rules) pages))
 
 (let [{:keys [rules updates]} (parse-input input)]
   (->> (remove #(correct-order? rules %) updates)
        (map #(fix-order rules %))
-       (sum-middle-page-numbers))) ; 4145
+       (aoc/sum aoc/middle))) ; 4145
  

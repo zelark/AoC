7100fff5f1feff0370f97d46d5647b1ca7392018
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2024-12-10 10:40:39 +0300

    [2024] Add Day 10

---
+++ src/zelark/aoc_2024/day_10.clj
@@ -0,0 +1,52 @@
+(ns zelark.aoc-2024.day-10
+  (:require [zelark.aoc.core :as aoc]
+            [zelark.aoc.grid-2d :as g2]
+            [zelark.aoc.graph :as g]))
+
+;; --- Day 10: Hoof It ---
+;; https://adventofcode.com/2024/day/10
+
+(def input (aoc/get-input 2024 10))
+
+(def ^:const lowest 0)
+(def ^:const highest 9)
+
+(defn parse-input [input]
+  (g2/parse input (g2/any-but g2/empty-space) aoc/ch->digit))
+
+(defn find-trailheads [grid]
+  (keys (filter (fn [[_ v]] (= v lowest)) grid)))
+
+(defn moves [grid from]
+  (for [to (g2/neighbors from)
+        :when (aoc/one? (- (grid to 100) (grid from)))]
+    to))
+
+(defn trailhead-score [grid loc]
+  (->> (g/bfs #(moves grid %) loc)
+       (filter #(= (grid (peek %)) highest))
+       (count)))
+
+(defn solve [part input]
+  (let [grid (parse-input input)
+        trailheads (find-trailheads grid)]
+    (transduce (map #(part grid %)) + trailheads)))
+
+;; part 1 (20.51833 msecs)
+(solve trailhead-score input) ; 510
+
+;; part 2 (26.975456 msecs)
+(defn seacrh [neighbors stack]
+  (when-let [path (peek stack)]
+    (cons path
+          (lazy-seq (seacrh neighbors
+                            (->> (neighbors (peek path))
+                                 (reduce #(conj %1 (conj path %2))
+                                         (pop stack))))))))
+
+(defn trailhead-rating [grid loc]
+  (->> (seacrh #(moves grid %) [[loc]])
+       (filter #(= (grid (peek %)) highest))
+       (count)))
+
+(solve trailhead-rating input) ; 1058

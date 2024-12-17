802e0662af17b4ef83a9f60ef1e90b67e551a46d
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2024-12-17 14:21:57 +0300

    [2024 Day 17] Polishing

--- src/zelark/aoc_2024/day_17.clj
+++ src/zelark/aoc_2024/day_17.clj
@@ -17,8 +17,7 @@
     (0 1 2 3) operand
     4 (state :a)
     5 (state :b)
-    6 (state :c)
-    7 (throw (ex-info "7 reserved and will not appear in valid programs." {:state state}))))
+    6 (state :c)))
 
 (defn run-code [regs code]
   (loop [state (assoc regs :out [])
@@ -26,37 +25,34 @@
     (let [opcode (nth code ip -1)]
       (if (neg? opcode)
         (:out state)
-        (let [literal (nth code (inc ip))
-              combo   (combo-operand state literal)]
+        (let [oprand (nth code (inc ip))
+              combo  (combo-operand state oprand)]
           (case opcode
             ;; adv
-            0  (let [ret (long (/ (:a state)
-                                  (bit-shift-left 1 combo)))]
-                 (recur (assoc state :a ret) (+ ip 2)))
+            0  (recur (assoc state :a (bit-shift-right (:a state) combo))
+                      (+ ip 2))
             ;; bxl
-            1  (let [ret (bit-xor (:b state) literal)]
-                 (recur (assoc state :b ret) (+ ip 2)))
+            1  (recur (assoc state :b (bit-xor (:b state) oprand))
+                      (+ ip 2))
             ;; bst
-            2  (let [ret (bit-and combo 2r111)]
-                 (recur (assoc state :b ret) (+ ip 2)))
+            2  (recur (assoc state :b (bit-and combo 2r111))
+                      (+ ip 2))
             ;; jnz
             3  (if (zero? (:a state))
                  (recur state (+ ip 2))
-                 (recur state literal))
+                 (recur state oprand))
             ;; bxc
-            4  (let [ret (bit-xor (:b state) (:c state))]
-                 (recur (assoc state :b ret) (+ ip 2)))
+            4  (recur (assoc state :b (bit-xor (:b state) (:c state)))
+                      (+ ip 2))
             ;; out
-            5  (let [ret (bit-and combo 2r111)]
-                 (recur (update state :out conj ret) (+ ip 2)))
+            5  (recur (update state :out conj (bit-and combo 2r111))
+                      (+ ip 2))
             ;; bdv
-            6  (let [ret (long (/ (:a state)
-                                  (bit-shift-left 1 combo)))]
-                 (recur (assoc state :b ret) (+ ip 2)))
+            6  (recur (assoc state :b (bit-shift-right (:a state) combo))
+                      (+ ip 2))
             ;; cdv
-            7  (let [ret (long (/ (:a state)
-                                  (bit-shift-left 1 combo)))]
-                 (recur (assoc state :c ret) (+ ip 2)))))))))
+            7  (recur (assoc state :c (bit-shift-right (:a state) combo))
+                      (+ ip 2))))))))
 
 ;; part 1 (1.541395 msecs)
 (let [{:keys [registers program]} (parse-input input)]
@@ -77,13 +73,12 @@
              (reduce conj (pop stack))
              (recur))))))
 
-;; Bouns
-;; We don't need it to solve. I did it just for fun.
-; 2,4, bst (mod a 8)  -> b
-; 1,1, bxl (xor b 1)  -> b
-; 7,5, cdv (/ a 2^b)  -> c
-; 1,5, bxl (xor b 5)  -> b
-; 0,3, adv (/ a 8)    -> a
-; 4,4, bxc (xor b c)  -> b
-; 5,5, out (mod b 8)  -> out
-; 3,0, jmp to 0 if a not zero (loop)
+;; An analyzed code to figure out what's going on here.
+; 2,4 - bst: (mod a 8)  -> b
+; 1,1 - bxl: (xor b 1)  -> b
+; 7,5 - cdv: (/ a 2^b)  -> c
+; 1,5 - bxl: (xor b 5)  -> b
+; 0,3 - adv: (/ a 2^3)  -> a
+; 4,4 - bxc: (xor b c)  -> b
+; 5,5 - out: (mod b 8)  -> out
+; 3,0 - jmp: to 0 if a not zero (loop)
